ee278497ecf16023b84b82f48c5f58b73f2268dd
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-05-21 20:51:46 -0300

    Python Sercer up and running

--- backend/app.py
+++ backend/app.py
@@ -10,43 +10,47 @@ app = Flask('__name__')
 CORS (app)
 
 # Set the route for uploading files 
-@app.route('/upload', method=['POST'])
+@app.route('/upload', methods=['POST'])
 # Define a function that wil hold all the logic for our file analysis 
 def upload_file():
     # Check if file is in request 
-    if 'file ' not in request.files:
+  if 'file ' not in request.files:
         # return json error 
-        return jsonify({'error: Error uploading file'}), 400 
+    return jsonify({'error: Error uploading file'}), 400 
    
    # Get the file requested 
-    file = request.files['file']
+  file = request.files['file']
     # Check the file has a valid name 
-    if file.filename == '':
+  if file.filename == '':
       # return errror 
-      return jsonify ({'error': 'No selected file uploaded'}), 400 
+    return jsonify ({'error': 'No selected file uploaded'}), 400 
 
-    try: 
-    # Read file in to panda DataFrame 
+  try:
+        # Read the file into a pandas DataFrame
         df = pd.read_csv(io.StringIO(file.stream.read().decode('UTF8')), header=0)
-      
-      # Perform PD analysis 
+        
+        # Extract LatStart and LongStart columns
+        if 'LatStart' in df.columns and 'LongStart' in df.columns:
+            # Convert the list of columns into a list of dictionaries 
+            # We do this so we can it can easily be sent to our FrontEnd as a JSON Response
+            locations = df[['LatStart', 'LongStart']].to_dict(orient='records')
+        else:
+            return jsonify({"error": "LatStart and LongStart columns not found"}), 400
+        
+        # Perform some basic analysis
         analysis = {
-        # some basic analysis 
-        "num_records" : len(df), 
-        "columns": list(df.columns),
-        "summary_statistics": df.describe().to_dict()
+            "num_records": len(df),
+            "columns": list(df.columns),
+            "summary_statistics": df.describe().to_dict(),
+            "locations": locations
         }
-    
-        # return success and give 200 OK 
-        return jsonify({'message':'File successfully read and analyzed', 'analysis': analysis}), 200 
-        # If error return error code 
-    except Exception as e:
-        return jsonify({'Error': str(e)}), 500 
-
+        
+        # Return the analysis results as JSON response 
+        return jsonify({"message": "File processed successfully", "analysis": analysis}), 200
+  except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-
-    # Run the app in debug for dev 
     app.run(debug=True)
 
 
