c7b01ab35cb604a78f4232fad222dacd28c9c706
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-05-21 22:57:33 -0300

    up to date

--- src/app/components/FileUpload.tsx
+++ src/app/components/FileUpload.tsx
@@ -2,16 +2,36 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone'
 
-// create a function that accepts a file
-// need to use OnDrop (built into React)
-// We need a DropZone to accept props
-// Then return it in a div and call it in our parent
+
+// Feed props to FileUpload that allow us to pass loacations and success messages to our function
+// This will allow the function to accept the Lat + Long from our Data file 
+// We will set a onSuccess message when the function executes with no errors 
+
 
 const FileUpload = () => {
-    const onDrop = useCallback((acceptedFiles: File[]) => {
+  // Set the state to handle messages and erorrs   
+  
+  
+  
+  
+  
+// Callback function to handle the file being dropped 
+// Create a new formData() --> Object to hold our file data 
+// APPEND !!! each file to the FormData 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     //This will be a logic for our file upload
     console.log(acceptedFiles)
     }, []);
+
+    
+    /**
+     * Make a POST request to our python backend using AXIOS 
+     * Following the route/then.response/catch format 
+     */
+
+
+
+
 
     const{ getRootProps, getInputProps } = useDropzone({onDrop})
 
