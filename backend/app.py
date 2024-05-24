c6702694af8763b44eeed173a55cb794c9ceb1fe
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-05-23 23:22:03 -0300

    Working upload errors

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import io
+import logging
+import openpyxl
 
 # Init the flask app, Start a server file for my app
 app = Flask('__name__')
@@ -9,36 +11,43 @@ app = Flask('__name__')
 # Enable CORS
 CORS(app)
 
+
+# Config for logging 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 # Define our Global Variable to store all our File Data in an empty dictionary
 geojson_data = {}
 
 # Function that will convert DataFrame to our GEOjson
 def df_to_geojson(df):
-    print(request.files) # debug  
     features = []
     for _, row in df.iterrows():
-        feature = {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',  
-                'coordinates': [row['LongStart'], row['LatStart']]
-            },
-            'properties': {
-                'CruiseIDstr': row['CruiseIDstr'],
-                'SampleLabel': row['SampleLabel'],
-                'WatchIDStr': row['WatchIDStr'],
-                'StartTime': row['StartTime'],
-                'Alpha': row['Alpha'],
-                'Distance': row['Distance'],
-                'FlySwim': row['FlySwim'],
-                'Count': row['Count'],
-                'Observer': row['Observer'],
-                'PlatformSpeed': row['PlatformSpeed'],
-                'Windspd': row['Windspd']
+        try:
+            feature = {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [row['LongStart'], row['LatStart']]
+                },
+                'properties': {
+                    'CruiseIDstr': row['CruiseIDstr'],
+                    'SampleLabel': row['SampleLabel'],
+                    'WatchIDStr': row['WatchIDStr'],
+                    'StartTime': row['StartTime'],
+                    'Alpha': row['Alpha'],
+                    'Distance': row['Distance'],
+                    'FlySwim': row['FlySwim'],
+                    'Count': row['Count'],
+                    'Observer': row['Observer'],
+                    'PlatformSpeed': row['PlatformSpeed'],
+                    'Windspd': row['Windspd']
+                }
             }
-        }
-        features.append(feature)
-
+            features.append(feature)
+        except KeyError as e:
+            logger.error(f"KeyError: {e}")
+            raise
     return {
         'type': 'FeatureCollection',
         'features': features
@@ -53,32 +62,48 @@ def home():
 # Define a function that will hold all the logic for our file analysis
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Check if file is in request
-    if 'file' not in request.files: 
-        # return json error
+    if 'file' not in request.files:
+        logger.error('No file part in request')
         return jsonify({'error': 'No file part'}), 400
-    
-    # Get the file requested
     file = request.files['file']
-    # Check the file has a valid name
     if file.filename == '':
-        # return error
+        logger.error('No selected file uploaded')
         return jsonify({'error': 'No selected file uploaded'}), 400
 
     try:
-        # Read the file into a pandas DataFrame
-        df = pd.read_csv(io.StringIO(file.stream.read().decode('UTF8')), header=0)
+        # Read the Excel file
+        excel_data = pd.ExcelFile(io.BytesIO(file.read()))
+        logger.debug(f"Sheet names in the uploaded file: {excel_data.sheet_names}")
+
+        # Check for required columns in each sheet
+        required_columns = {'LongStart', 'LatStart', 'CruiseIDstr', 'SampleLabel', 'WatchIDStr', 
+                            'StartTime', 'Alpha', 'Distance', 'FlySwim', 'Count', 'Observer', 
+                            'PlatformSpeed', 'Windspd'}
         
-        # Convert our DataFrame into geoJSON
+        sheet_name = None
+        for sheet in excel_data.sheet_names:
+            df = pd.read_excel(excel_data, sheet_name=sheet)
+            if required_columns.issubset(set(df.columns)):
+                sheet_name = sheet
+                break
+
+        if sheet_name is None:
+            logger.error('No sheet contains the required columns')
+            return jsonify({'error': 'No sheet contains the required columns.'}), 400
+
+        df = pd.read_excel(excel_data, sheet_name=sheet_name)
+        logger.debug(f"DataFrame loaded from sheet '{sheet_name}'")
+
         global geojson_data
         geojson_data = df_to_geojson(df)
+        logger.debug(f"GeoJSON data prepared: {geojson_data}")
 
-        # Return the geoJSON data
         return jsonify({"message": "File processed successfully", "geojson": geojson_data}), 200
     except Exception as e:
+        logger.error(f"Exception occurred: {e}")
         return jsonify({"error": str(e)}), 500
 
-# Define our geojson endpoint that has our stored data
+
 @app.route('/get-geojson', methods=['GET'])
 def get_geojson():
     return jsonify(geojson_data)
--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -2,49 +2,51 @@ import unittest
 import io
 import sys
 import os
+from flask_testing import TestCase
+from app import app  
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app  # Import your Flask app
+from app import app  
 
 from flask_testing import TestCase
 
 class TestSeabirdSurveyApp(TestCase):
 
+    # Bind the app to the current context
     def create_app(self):
-        # Bind the app to the current context
         app.config['TESTING'] = True
         return app
 
+    # Test the home route
     def test_home(self):
-        # Test the home route
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Welcome to the Seabird Survey App API.', response.data)
-        print('Test 2 : API Running')
+        print('API Running : Test 1 Pass ')
 
+    # Test the file upload route
     def test_upload_file(self):
-        # Test the file upload route
         data = {
             'file': (io.BytesIO(b"LatStart,LongStart,CruiseIDstr,SampleLabel,WatchIDStr,StartTime,Alpha,Distance,FlySwim,Count,Observer,PlatformSpeed,Windspd\n45.0103,44.9872,Cruise123,SampleA,Watch1,08:00,Alpha1,100,Flying,5,Observer1,10,15"), 'test.csv')
         }
         response = self.client.post('/upload', content_type='multipart/form-data', data=data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'File processed successfully', response.data)
-        print('Test 3: File processed successfully')
+        print('File processed successfully: Test 2 Pass')
 
+    # Test the get-geojson route
     def test_get_geojson(self):
-        # Test the get-geojson route
         data = {
             'file': (io.BytesIO(b"LatStart,LongStart,CruiseIDstr,SampleLabel,WatchIDStr,StartTime,Alpha,Distance,FlySwim,Count,Observer,PlatformSpeed,Windspd\n45.0103,44.9872,Cruise123,SampleA,Watch1,08:00,Alpha1,100,Flying,5,Observer1,10,15"), 'test.csv')
         }
         self.client.post('/upload', content_type='multipart/form-data', data=data)
 
-        # test the get-geojson route
         response = self.client.get('/get-geojson')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'FeatureCollection', response.data)
-        print('Test 1: get-geojson successfull')
+        print('get-geojson successfull: Test 3 Pass')
 
+# Run Test 
 if __name__ == '__main__':
     unittest.main()
